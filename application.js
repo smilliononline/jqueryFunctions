c7ec4deb69704754f4cb2f5e4fe8355a87fb828f
smilliononline <million_18@outlook.com>
2018-05-28 23:45:08 -0500

    update

--- application.js
+++ application.js
@@ -7,8 +7,23 @@ $(document).ready(function () {
    });
    $('button').click(function(){
        $('#derp').show();
-   })
-   
+   });
+   $('h1').click(function(){
+       $(this).toggle();
+    });
+    $('#blurp').hover(function (){
+        $(this).fadeOut(100);
+        $(this).fadeIn(500);
+    });
+    $('h2').addClass('woop');
+
+    $('h1').before("<h1>HEY ITS ME AGAIN!</h1>");
+
+    $('li').last().append(" Holy Smokes!");
+
+    $("#hoot").html('<span style="color:red"> HOOT </span>');
+
+    $('#durg').text("Hey i am changing the text..what's up now JQuery?? ya.");
 
   });
   
