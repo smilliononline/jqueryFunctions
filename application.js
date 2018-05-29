e8ce27d88cd50dd9eba5b02abc24557f167e5150
smilliononline <million_18@outlook.com>
2018-05-28 22:05:34 -0500

    update

--- application.js
+++ application.js
@@ -2,6 +2,13 @@ $(document).ready(function () {
    $('button').click(function(){
        alert("WHATTT!");
    });
+   $('button').click(function() {
+       $('#derp').hide();
+   });
+   $('button').click(function(){
+       $('#derp').show();
+   })
+   
 
   });
   
